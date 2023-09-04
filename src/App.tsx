387b90db0b5a1597f29ad7ab68c736dd1921b208
Dravid24 <dravidmani1998@gmail.com>
2023-09-04 21:47:12 +0530

    Azure, embedded analytics, others, power platform, react portfolio, skill matrix embedded code integration

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,16 @@ import "./App.css";
 import Layout from "./Layout";
 import Profile from "./components/Profile";
 import Tableau from "./components/Tableau";
-import UnderConstruction from "./components/UnderConstruction";
 import PowerBIComponent from "./components/PowerBI";
 import { PowerBIDetail } from "./components/PowerBIDetail";
 import DomoComponent from "./components/DomoComponent";
 import ApacheComponent from "./components/ApacheComponent";
+import Azure from "./components/Azure";
+import PowerPlatform from "./components/PowerPlatform";
+import ReactPortfolio from "./components/ReactPortfolio";
+import EmbeddedAnalytics from "./components/EmbeddedAnalytics";
+import SkillMatrix from "./components/SkillMatrix";
+import OtherSkill from "./components/OtherSkill";
 
 const App = () => {
   return (
@@ -21,12 +26,12 @@ const App = () => {
             <Route path={"power-bi/:name"} element={<PowerBIDetail />} />
             <Route path="/domo" element={<DomoComponent />} />
             <Route path="/apache" element={<ApacheComponent />} />
-            <Route path="/azure" element={<UnderConstruction />} />
-            <Route path="/power-platform" element={<UnderConstruction />} />
-            <Route path="/react-portfolio" element={<UnderConstruction />} />
-            <Route path="/embedded" element={<UnderConstruction />} />
-            <Route path="/skill" element={<UnderConstruction />} />
-            <Route path="/others" element={<UnderConstruction />} />
+            <Route path="/azure" element={<Azure />} />
+            <Route path="/power-platform" element={<PowerPlatform />} />
+            <Route path="/react-portfolio" element={<ReactPortfolio />} />
+            <Route path="/embedded" element={<EmbeddedAnalytics />} />
+            <Route path="/skill" element={<SkillMatrix />} />
+            <Route path="/others" element={<OtherSkill />} />
           </Routes>
         </Layout>
       </Router>
